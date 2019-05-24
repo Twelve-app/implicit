6fa97ce82647d7f57bea88588599c793cc6d79df
sam <sam@yellw.co>
2019-05-24 19:48:30 +0200

    Add module to init

--- implicit/__init__.py
+++ implicit/__init__.py
@@ -1,10 +1,11 @@
 from .als import alternating_least_squares
 
 from . import als
+from . import als_partial
 from . import approximate_als
 from . import bpr
 from . import nearest_neighbours
 
 __version__ = '0.3.8'
 
-__all__ = [alternating_least_squares, als, approximate_als, bpr, nearest_neighbours, __version__]
+__all__ = [alternating_least_squares, als, als_partial, approximate_als, bpr, nearest_neighbours, __version__]
